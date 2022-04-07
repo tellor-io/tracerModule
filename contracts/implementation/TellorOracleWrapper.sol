4ab7220fe3762ad7de8b36ee76859bb22b01890e
Quentin <thealmightyquinn@hotmail.com>
2022-04-07 14:10:52 +0200

    Update TellorOracleWrapper.sol

--- contracts/implementation/TellorOracleWrapper.sol
+++ contracts/implementation/TellorOracleWrapper.sol
@@ -14,16 +14,19 @@ contract TellorOracleWrapper is IOracleWrapper, UsingTellor {
     address public immutable override deployer;
     uint8 private constant MAX_DECIMALS = 18;
     int256 public scaler;
+    bytes32 public queryId;
 
     // #### Functions
     constructor(
         address payable _oracle,
-        address _deployer
+        address _deployer,
+        uint256 _poolId
     ) UsingTellor(_oracle) {
         require(_oracle != address(0), "Oracle cannot be null");
         require(_deployer != address(0), "Deployer cannot be null");
         oracle = _oracle;
         deployer = _deployer;
+        queryId = _getQueryId(_poolId);
         // reset the scaler for consistency
         uint8 _decimals = IOracleWrapper(oracle).decimals();
         require(_decimals <= MAX_DECIMALS, "COA: too many decimals");
@@ -40,34 +43,17 @@ contract TellorOracleWrapper is IOracleWrapper, UsingTellor {
     /**
      * @notice Returns the oracle price in WAD format
      */
-    function getPrice(bytes32 _queryId, uint256 _timestamp) external view returns (int256) {
-        (int256 _price, ) = _readPriceBefore(_queryId, _timestamp);
+    function getPrice() external view override returns (int256) {
+        (int256 _price, ) = _readPriceBefore(queryId, block.timestamp - 5 minutes);
         return _price;
     }
-
-        /**
-     * @notice Returns the current price for the asset in question
-     * @return The latest price
-     */
-    function getPrice() external pure override returns (int256){
-        return 0;
-    }
-
-    /**
-     * @return _price The latest round data price
-     * @return _data The metadata. Implementations can choose what data to return here
-     */
-    function getPriceAndMetadata() external pure override returns (int256 _price, bytes memory _data){
-        return (0, bytes("0"));
-    }
-
 
     /**
      * @return _price The latest price
      * @return _data The metadata. Implementations can choose what data to return here. This implementation returns the timestamp
      */
-    function getPriceAndMetadata(bytes32 _queryId, uint256 _timestamp) external view returns (int256, bytes memory) {
-        (int256 _price, uint256 _timestampReceived) = _readPriceBefore(_queryId, _timestamp);
+    function getPriceAndMetadata() external view override returns (int256, bytes memory) {
+        (int256 _price, uint256 _timestampReceived) = _readPriceBefore(queryId, block.timestamp - 5 minutes);
         bytes memory _data = abi.encodePacked(_timestampReceived);
         return (_price, _data);
     }
@@ -84,6 +70,13 @@ contract TellorOracleWrapper is IOracleWrapper, UsingTellor {
             _price := mload(add(_value, 0x20))
         }
         return (toWad(_price), _timestampReceived);
+    }
+
+    // Tellor query
+    function _getQueryId(uint256 _poolId) internal view returns (bytes32) {
+        bytes memory _b = abi.encode("TracerFinance", abi.encode(_poolId));
+        bytes32 _queryId = keccak256(_b);
+        return _queryId;
     }
 
     /**
